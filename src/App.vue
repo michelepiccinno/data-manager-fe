e4b48570b290049daa25316baba6620f65ffc752
michelepiccinno <michele.piccinno@gmail.com>
2024-04-30 19:35:56 +0200

    fix fontawesome

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  <script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import HelloWorld from './components/HelloWorld.vue'
 
-</script>
+  </script>
 
-<template>
-  <div class="container-fluid pt-5">
-    <div class="row">
-      <div class="col-4">
-        <h1>test</h1>
-        <div>
-          <i class="fa-solid fa-user"></i>
- 
+  <template>
+    <div class="container-fluid pt-5">
+      <div class="row">
+        <div class="col-4">
+          <h1>test</h1>
+          <div>
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-solid fa-phone"></i>
+            <i class="fa-solid fa-user"></i>
+            <i class="fa-solid fa-square-phone"></i>
+            <i class="fa-solid fa-camera"></i>
+            <i class="fa-solid fa-camera"></i>
+            <i class="fa-brands fa-facebook"></i>
 
-      </div>
+        </div>
+        </div>
       </div>
     </div>
-  </div>
-</template>
+  </template>
 
-<style scoped></style>
+  <style scoped></style>
