04a405cc5bfa7b3fbc95b71a147f62d93ae5a93a
michelepiccinno <michele.piccinno@gmail.com>
2024-06-03 16:23:28 +0200

    add laravel backend + api call test

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
   <script>
   import { RouterLink, RouterView } from 'vue-router'
   import TicketsTable from './components/TicketsTable.vue';
-import TheHeader from './pages/TheHeader.vue';
+  import TheHeader from './pages/TheHeader.vue';
+  import axios from 'axios';
 
   export default {
     components: {
       TicketsTable,
       TheHeader
     },
+    data() {
+      return {
+        info: null
+      }
+    },
+    mounted() {
+      axios
+        .get('http://127.0.0.1:8000/api/welcome')
+        .then(response => (this.info = response))
+    }
   }
 </script>
 
 <template>
   <TheHeader />
   <router-view />
-
+  {{ info }}
 
 </template>
 
